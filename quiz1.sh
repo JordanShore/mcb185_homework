0d93386c5bed1d7c97521d362103506267c0131f
username <jordanshuaihengshore@gmail.com>
2024-01-23 09:26:45 -0800

    task4

--- quiz1.sh
+++ quiz1.sh
@@ -1,3 +1,6 @@
 Jordan Shore
 JordanShore
 gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -E -v "[^ozniacr]" | grep -E "r+"  | grep -E ".{4,}"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -E -v "[^tairnlb]" | grep -E "b+"  | grep -E ".{4,}"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -E -v "[^maodinc]" | grep -E "c+"  | grep -E ".{4,}"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -E -v "[^anoigrz]" | grep -E "z+"  | grep -E ".{4,}"
