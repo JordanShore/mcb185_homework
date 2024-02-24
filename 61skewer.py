58f708ca72e11b8a9773160ce4f0a061afafa7cf
JordanShore <jsshore@ucdavis.edu>
2024-02-23 16:42:34 -0800

    update

--- 61skewer.py
+++ 61skewer.py
@@ -2,9 +2,37 @@
 #
 #This program prints gc_comp and gc_skew for a sequence
 import dogma
-
+import sys
+import gzip
+'''
 seq = 'ACGTACGTGGGGGACGTACGTCCCCC'
 w = 10
 for i in range(len(seq) -w +1):
 	s = seq[i:i+w]
 	print(i, dogma.gc_comp(s), dogma.gc_skew(s))
+'''
+
+path = sys.argv[1]
+windowsize = int(sys.argv[2])
+
+with gzip.open(path, "rt") as file:
+	count = 0
+	seq = ""
+	for line in file:
+		#IF statement uses count to skip the first line
+		count += 1
+		if count<2:
+			continue
+		else:
+		#Iterates through each nucleotide in the 81 nucleotide lines
+		#Appends them to the nucleotidelist
+			line = line.strip()
+			for nuc in line:
+					seq = seq + nuc
+
+
+for i in range(len(seq) -windowsize +1):
+	s = seq[i:i+windowsize]
+	currentgc_comp = dogma.gc_comp(s)
+	currentgc_skew = dogma.gc_skew(s)
+	print(i, currentgc_comp, currentgc_skew)
--- 62skewer.py
+++ 62skewer.py
@@ -3,66 +3,61 @@
 #This program prints gc_comp and gc_skew for a sequence.
 import dogma
 import gzip
+import sys
 
-path = "GCF_000005845.2_ASM584v2_genomic.fna.gz"
+path = sys.argv[1]
+windowsize = int(sys.argv[2])
+
 with gzip.open(path, "rt") as file:
 	count = 0
 	seq = ""
 	for line in file:
-		#IF statement uses count to skip the first 1000 lines
+		#IF statement uses count to skip the first line
 		count += 1
-		if count<1000:
+		if count<2:
 			continue
-		#Adds a total of 1000 nucleotides
-		elif len(seq)>1000:
-			break
 		else:
 		#Iterates through each nucleotide in the 81 nucleotide lines
 		#Appends them to the nucleotidelist
 			line = line.strip()
 			for nuc in line:
-				seq = seq + nuc
-#print(seq)
+					seq = seq + nuc
 
+#windowsize of 10 as in HW61
+#This chunk prints the starting gc_comps and skew using dogma
 start = 0
-end = 3
+end = start + windowsize
 window = seq[start:end]
-windowsize = 10
 start_gccomp = dogma.gc_comp(window)
 start_gcskew = dogma.gc_skew(window)
-print(start_gccomp)
-print(start_gcskew)
+print(0, start_gccomp, start_gcskew)
 
 ccount = window.count('C') 
 gcount = window.count('G')
 
-current_gccomp = (ccount + gcount) / len(window)
-	if ccount == gcount:
-		current_gcskew = 0
-	else:
-		current_gcskew = (g - c) / (g + c)
-
-for i in range(1000):
+for i in range(len(seq)-windowsize):
 
 	if window[0] == 'C':
 		ccount -= 1
 	elif window[0] == 'G':
 		gcount -= 1
 	
+	start += 1
+	end += 1 
+	window = seq[start:end]
+
+	if window[-1] == 'C':
+		ccount += 1
+	elif window[-1] == 'G':
+		gcount += 1 
+
+	current_gccomp = (ccount + gcount) / len(window)
+	if ccount == gcount:
+		current_gcskew = 0
+	else:
+		current_gcskew = (gcount - ccount) / (gcount + ccount)
+
+	print(i, current_gccomp, current_gcskew)
 
 
-
-
-'''
-#61skewer.py by Jordan Shore
-#
-#This program prints gc_comp and gc_skew for a sequence
-import dogma
-
-seq = 'ACGTACGTGGGGGACGTACGTCCCCC'
-w = 10
-for i in range(len(seq) -w +1):
-	s = seq[i:i+w]
-	print(i, dogma.gc_comp(s), dogma.gc_skew(s))
-'''
 
