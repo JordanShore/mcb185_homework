7ee68f2ca14c9f969ebd387ea7c755741f33437f
JordanShore <jsshore@ucdavis.edu>
2024-02-23 21:53:19 -0800

    update

--- 63dust.py
+++ 63dust.py
@@ -6,9 +6,11 @@ import dogma
 import gzip
 import sys
 
+#Command line inputs
 path = sys.argv[1]
 windowsize = int(sys.argv[2])
-enthreshold = sys.argv[3]
+enthreshold = float(sys.argv[3])
+
 
 #Opening file and creating the sequence
 with gzip.open(path, "rt") as file:
@@ -26,21 +28,33 @@ with gzip.open(path, "rt") as file:
 			for nuc in line:
 					seq = seq + nuc
 
-#windowsize of 10 as in HW61
-#This chunk prints the starting gc_comps and skew using dogma
+
+#This chunk creates the starting state of window and finalseq
+
 start = 0
 end = start + windowsize
 window = seq[start:end]
 finalseq = window
+
+
+#The following does the first check for a low entropy window
+#On only the first window
 
 acount = window.count('A')
 ccount = window.count('C') 
 gcount = window.count('G')
 tcount = window.count('T')
 entropy = dogma.shannon_entropy(acount,ccount,gcount,tcount)
+
+
+#For loop just building a string of Ns the size of the window
+
 nstring = ""
 for i in range(windowsize):
 	nstring += "N"
+
+
+#Make the first window into Ns 
 
 if entropy < enthreshold:
 	finalseq = nstring
@@ -59,13 +73,23 @@ for i in range(len(seq)-windowsize):
 	end += 1 
 	window = seq[start:end]
 
+
+#Now we append one at a time, either N or the nucleotide
+
 	if entropy < enthreshold:
 		finalseq += "N"
 	else:
 		finalseq += window[-1]
 
+
 print(finalseq)
 
+
+
+#Print like a FASTA file
+print(title, end = "")
+for i in range(0,len(finalseq),60):
+	print(finalseq[i:i+60])
 
 
 
