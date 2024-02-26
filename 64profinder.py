02998795e85b80751906ab6096babe94ad8f35c7
JordanShore <jsshore@ucdavis.edu>
2024-02-25 16:42:36 -0800

    update

--- 64profinder.py
+++ 64profinder.py
@@ -15,12 +15,12 @@ with gzip.open(path, "rt") as file:
 	count = 0
 	seq = ""
 	for line in file:
-#IF statement uses count to skip the first line
+#IF statement uses count to make the first line a title
 		count += 1
 		if count == 1:
 			title = line
 		#For testing with less than whole genome.
-		#elif len(seq)>20000:
+		#elif len(seq)>2000:
 			#break
 		else:
 #Iterates through each nucleotide in the 81 nucleotide lines
@@ -30,32 +30,53 @@ with gzip.open(path, "rt") as file:
 					seq = seq + nuc
 
 
-
+#Creates reverse complement.
 rseq = dogma.revcomp(seq)
 
+#Creates the reading frames, 3 forward 3 back and puts them in a list.
 fseq1,fseq2,fseq3 = dogma.orfs(seq)
 rseq1,rseq2,rseq3 = dogma.orfs(rseq)
-
 sequencelist = [fseq1,fseq2,fseq3,rseq1,rseq2,rseq3]
+
+#The first part of title list will be used for protein titles.
 titlelist = title.split()
 
+#Start at protein 1 when printing proteins found later.
 numprotein = 1
-for seq in sequencelist:
-	temptranscript = dogma.translate(seq)
 
+#FOR loop iterates through the 3 forward and reverse reading frames.
+for seq in sequencelist:
+#Translates the DNA sequence to amino acids
+	temptranscript = dogma.translate(seq)
+#Initialize where the start and ends of proteins may be.
 	potentialstart = 0
 	potentialend = 0
 
+	'''
+WHILE loop evaluates all the potential proteins in an aa transcript.
+Each iteration evaluates a single possible protein.
+
+potentialstart/end are found by .find("M") or .find("*")
+These represent Methionine and the STOP codon.
+
+If .find() ever doesn't find a start or stop it will return -1, ending the loop.
+
+potentialend - potentialstart signifies protein length.
+IF statement prints protein information and increases numprotein by 1.
+numprotein is just signifying how many proteins you've found.
+
+temptranscript if lastly changed to only be everything after the latest "*",
+this way we move forward through the transcript to the next possible protein.
+	'''
 	while (potentialstart != -1) and (potentialend != -1):
 		potentialstart = temptranscript.find("M")
-		rest = temptranscript[potentialstart:]
-		potentialend = rest.find("*")
+		potentialend = temptranscript.find("*")
 
-		if potentialend >= minprotein_size:
+		if potentialend - potentialstart >= minprotein_size:
 			print(titlelist[0]+"-prot-"+str(numprotein))
-			print(temptranscript[potentialstart:potentialstart+potentialend])
+			print(temptranscript[potentialstart:potentialend])
 			numprotein += 1
 		
-		temptranscript = temptranscript[potentialstart+potentialend:]
+		temptranscript = temptranscript[potentialend+1:]
 
 
