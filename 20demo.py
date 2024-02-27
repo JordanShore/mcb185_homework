2d06d5efe0cf24459c24c4d9a6009e01d8e8dff3
Jordan <jsshore@ucdavis.edu>
2024-02-26 16:12:09 -0800

    Unit6

--- 20demo.py
+++ 20demo.py
@@ -1,3 +1,17 @@
 #20demo.py by Jordan Shore
 
+import math
+import sys
+
 print('hello, again')
+print(1.5e-2)
+print(1.5*math.pow(10,-2))
+print(math.log10(100))
+print(float("inf"))
+print(math.inf)
+
+x=10
+assert(x<11)
+
+if (x<=11):
+	sys.exit("Sys Exit 11")
